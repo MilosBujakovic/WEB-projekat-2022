e2c8848d4f1568e39b6460bf43878f611f61d5cb
MilosBujakovic <milosbujakovic97@gmail.com>
2022-09-08 22:26:20 +0200

    dodata zastita za prazne liste

--- src/repozitorijumi/UnosServis.java
+++ src/repozitorijumi/UnosServis.java
@@ -62,76 +62,93 @@ public class UnosServis
 	private List<Objava> stringUobjave(String str, int nivoDelimitera)
 	{
 		List<Objava> objaveKorisnika = new ArrayList<Objava>();
-		String podaci[] = str.split(delimiteri[nivoDelimitera]);
-		for(String profilna : podaci)
+		if(!str.equals("") && str!=null)
 		{
-			objaveKorisnika.add(stringUobjavu(profilna, nivoDelimitera+1));
+			String podaci[] = str.split(delimiteri[nivoDelimitera]);
+			for(String profilna : podaci)
+			{
+				objaveKorisnika.add(stringUobjavu(profilna, nivoDelimitera+1));
+			}
 		}
 		return objaveKorisnika;
 	}
 
 	private Objava stringUobjavu(String str, int nivoDelimitera)
 	{
-		String podaci[] = str.split(delimiteri[nivoDelimitera]);
-		if(Status.valueOf(podaci[2])==Status.AKTIVAN)
+		if(!str.equals("") && str!=null)
 		{
-			Objava objava = new Objava();
-			objava.setId(Integer.parseInt(podaci[0]));
-			objava.setKorisnickoIme(podaci[1]);
-			objava.setStatus(Status.valueOf(podaci[2]));
-			objava.setSlika(podaci[3]);
-			objava.setTekst(podaci[4]);
-			objava.setKomentari(stringUkomentare(podaci[5], nivoDelimitera+1));
-			return objava;
+			String podaci[] = str.split(delimiteri[nivoDelimitera]);
+			if(Status.valueOf(podaci[2])==Status.AKTIVAN)
+			{
+				Objava objava = new Objava();
+				objava.setId(Integer.parseInt(podaci[0]));
+				objava.setKorisnickoIme(podaci[1]);
+				objava.setStatus(Status.valueOf(podaci[2]));
+				objava.setSlika(podaci[3]);
+				objava.setTekst(podaci[4]);
+				objava.setKomentari(stringUkomentare(podaci[5], nivoDelimitera+1));
+				return objava;
+			}
+			else return null;
 		}
-		else return null;
+		return null;
 	}
 	
 	private List<ProfilnaSlika> stringUprofilne(String str, int nivoDelimitera)
 	{
 		List<ProfilnaSlika> profilneKorisnika = new ArrayList<ProfilnaSlika>();
-		String podaci[] = str.split(delimiteri[nivoDelimitera]);
-		for(String profilna : podaci)
+		if(!str.equals("") && str!=null)
 		{
-			profilneKorisnika.add(stringUprofilnu(profilna, nivoDelimitera+1));
+			String podaci[] = str.split(delimiteri[nivoDelimitera]);
+			for(String profilna : podaci)
+			{
+				profilneKorisnika.add(stringUprofilnu(profilna, nivoDelimitera+1));
+			}
 		}
 		return profilneKorisnika;
 	}
 	
 	public ProfilnaSlika stringUprofilnu(String str, int nivoDelimitera)
 	{
-		String podaci[] = str.split(delimiteri[nivoDelimitera]);
-		if(Status.valueOf(podaci[2])==Status.AKTIVAN)
+		if(!str.equals("") && str!=null)
 		{
-			ProfilnaSlika profilna = new ProfilnaSlika();
-			profilna.setId(Integer.parseInt(podaci[0]));
-			profilna.setKorisnickoIme(podaci[1]);
-			profilna.setStatus(Status.valueOf(podaci[2]));
-			profilna.setLinkSlike(podaci[2]);
-			profilna.setOpis(podaci[3]);
-			profilna.setKomentari(stringUkomentare(podaci[4], nivoDelimitera+1));
-			return profilna;
+			String podaci[] = str.split(delimiteri[nivoDelimitera]);
+			if(Status.valueOf(podaci[2])==Status.AKTIVAN)
+			{
+				ProfilnaSlika profilna = new ProfilnaSlika();
+				profilna.setId(Integer.parseInt(podaci[0]));
+				profilna.setKorisnickoIme(podaci[1]);
+				profilna.setStatus(Status.valueOf(podaci[2]));
+				profilna.setLinkSlike(podaci[2]);
+				profilna.setOpis(podaci[3]);
+				profilna.setKomentari(stringUkomentare(podaci[4], nivoDelimitera+1));
+				return profilna;
+			}
 		}
 		return null;
 	}
 
 	private List<Komentar> stringUkomentare(String str, int nivoDelimitera)
 	{
-		String podaci[] = str.split(delimiteri[nivoDelimitera]);
 		List<Komentar> komentari = new ArrayList<Komentar>();
-		for(String podatak : podaci)
+		if(!str.equals("") && str!=null)
 		{
-			String opaska[] = podatak.split(delimiteri[nivoDelimitera+1]);
-			if(Status.valueOf(podaci[2])==Status.AKTIVAN)
+			String podaci[] = str.split(delimiteri[nivoDelimitera]);
+			
+			for(String podatak : podaci)
 			{
-				Komentar komentar = new Komentar();
-				komentar.setId(Integer.parseInt(opaska[0]));
-				komentar.setKorisnickoIme(opaska[1]);
-				komentar.setStatus(Status.valueOf(podaci[2]));
-				komentar.setTekst(opaska[3]);
-				komentar.setDatumKomentara(opaska[4]);
-				komentar.setDatumIzmjene(opaska[5]);
-				komentari.add(komentar);
+				String opaska[] = podatak.split(delimiteri[nivoDelimitera+1]);
+				if(Status.valueOf(podaci[2])==Status.AKTIVAN)
+				{
+					Komentar komentar = new Komentar();
+					komentar.setId(Integer.parseInt(opaska[0]));
+					komentar.setKorisnickoIme(opaska[1]);
+					komentar.setStatus(Status.valueOf(podaci[2]));
+					komentar.setTekst(opaska[3]);
+					komentar.setDatumKomentara(opaska[4]);
+					komentar.setDatumIzmjene(opaska[5]);
+					komentari.add(komentar);
+				}
 			}
 		}
 		return komentari;
@@ -140,17 +157,21 @@ public class UnosServis
 	public List<ZahtjevZaPrijateljstvo> stringUzahtjeve(String str, int nivoDelimitera)
 	{
 		List<ZahtjevZaPrijateljstvo> listaZahtjeva= new ArrayList<ZahtjevZaPrijateljstvo>();
-		String zahtjevi[] = str.split(delimiteri[nivoDelimitera]);
-		for(String podaci : zahtjevi)
+		
+		if(!str.equals("") && str!=null)
 		{
-			String podatak[] = podaci.split(delimiteri[nivoDelimitera+1]);
-			ZahtjevZaPrijateljstvo zahtjev = new ZahtjevZaPrijateljstvo();
-			zahtjev.setId(Integer.parseInt(podatak[0]));
-			zahtjev.setPosiljalac(podatak[1]);
-			zahtjev.setPrimalac(podatak[2]);
-			zahtjev.setDatum(podatak[3]);
-			zahtjev.setStatus(StatusZahtjeva.valueOf(podatak[4]));
-			listaZahtjeva.add(zahtjev);
+			String zahtjevi[] = str.split(delimiteri[nivoDelimitera]);
+			for(String podaci : zahtjevi)
+			{
+				String podatak[] = podaci.split(delimiteri[nivoDelimitera+1]);
+				ZahtjevZaPrijateljstvo zahtjev = new ZahtjevZaPrijateljstvo();
+				zahtjev.setId(Integer.parseInt(podatak[0]));
+				zahtjev.setPosiljalac(podatak[1]);
+				zahtjev.setPrimalac(podatak[2]);
+				zahtjev.setDatum(podatak[3]);
+				zahtjev.setStatus(StatusZahtjeva.valueOf(podatak[4]));
+				listaZahtjeva.add(zahtjev);
+			}
 		}
 		return listaZahtjeva;
 		
